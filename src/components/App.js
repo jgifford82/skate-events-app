1a84e81962caaf513466b64b4a0e6a0db8a617b8
Jennifer Gifford <jgifford82@gmail.com>
2022-09-11 15:30:42 -0600

    render NavBar in App, import material UI AppBar to set up basic NavBar code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import "../App.css";
+import React from "react";
+import NavBar from "./NavBar";
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <NavBar />
     </div>
   );
-}
+};
 
 export default App;
