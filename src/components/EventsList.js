5de239fd989e8c76f2e355541f7f0b27e8132281
Jennifer Gifford <jgifford82@gmail.com>
2022-09-12 18:00:01 -0600

    add key to renderEvents map

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -3,9 +3,9 @@ import React from "react";
 
 const EventsList = ({ meets }) => {
   //   console.log(meets);
-  const renderEvents = meets.map((meet) => <li>{meet.title}</li>);
+  const renderEvents = meets.map((meet) => <li key={meet.id}>{meet.title}</li>);
 
-  return <div>EventsList Placeholder {renderEvents}</div>;
+  return <div>{renderEvents}</div>;
 };
 
 export default EventsList;
