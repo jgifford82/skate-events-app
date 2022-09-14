a42b07b1047230740d86a4f5e05b955df4dd621d
Jennifer Gifford <jgifford82@gmail.com>
2022-09-13 20:57:14 -0600

    set up controlled form inputs

--- src/components/EventsForm.js
+++ src/components/EventsForm.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EventsForm = () => {
+  const initialValues = {
+    title: "",
+    day: "",
+    time: "",
+    location: "",
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  const handleInputChange = (e) => {
+    //const name = e.target.name
+    //const value = e.target.value
+    const { name, value } = e.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+    console.log(values);
+  };
+
   function handleSubmit(event) {
+    // prevent page refresh on submit:
     event.preventDefault();
     console.log("submitted");
+    // clear input fields on submit by updating values state:
+    setValues(initialValues);
   }
 
   return (
@@ -12,10 +36,34 @@ const EventsForm = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Add New Event:
-          <input type="text" name="title" placeholder="Event Title" />
-          <input type="text" name="day" placeholder="Day of the week" />
-          <input type="text" name="time" placeholder="Time frame" />
-          <input type="text" name="location" placeholder="Location" />
+          <input
+            type="text"
+            name="title"
+            placeholder="Event Title"
+            value={values.title}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="day"
+            placeholder="Day of the week"
+            value={values.day}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="time"
+            placeholder="Time frame"
+            value={values.time}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="location"
+            placeholder="Location"
+            value={values.location}
+            onChange={handleInputChange}
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>
